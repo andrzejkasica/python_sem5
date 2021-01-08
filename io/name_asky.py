adc0a875811db5bb11d1c19b779cb7373bbadeec
andrzejkasica <48736499+andrzejkasica@users.noreply.github.com>
2021-01-08 18:37:31 +0100

    Add files via upload

---
+++ io/name_asky.py
@@ -0,0 +1,7 @@
+name, surname, birth = input("Podaj imie, nazwisko i date urodzenia, oddziel kolejne wyrazy spacja: ").split(" ")
+
+print("imie: ", name)
+print("nazwisko ", surname)
+print("dzien urodzenia ", birth)
+
+
---
+++ io/szyfr.py
@@ -0,0 +1,8 @@
+szyfr = input("Podaj szyfr: ")
+
+szyfr2 = input("Podaj szyfr 2 raz: ")
+
+if szyfr == szyfr2:
+    print("Prawidłowo")
+else:
+    print("Kod niepoprawny")
